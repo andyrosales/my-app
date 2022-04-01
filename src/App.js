5637327dceae76433e7a2097974f8430617ca147
Andre Rosales <andyrosales529@gmail.com>
2022-04-01 08:57:08 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I think I finally figured out how to do this stuff lol
+        </p>
+        <div>
+          Hi Elias
+        </div>
+        <a
+          className="App-link"
+          href="https://youtu.be/dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Totally not a Rick Roll
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
